f539d515becb541b33bab49fab655329a1f97264
Ignacio Barcelo <ignacio.barcelo@poliglota.org>
2021-09-28 13:51:20 -0300

    added styles for pokedex and page for pokemon details

---
+++ pages/pokemon.js
@@ -0,0 +1,43 @@
+import React from "react";
+import Layout from "../components/Layout";
+import Link from "next/link";
+
+export default function pokemon({ pokeman }) {
+  return (
+    <Layout title={pokeman.name}>
+      <h1 className="text-4xl mb-2 text-center capitalize"> {pokeman.name} </h1>
+      <img className="mx-auto" src={pokeman.image} alt={pokeman.name} />
+      <p className="font-bold mr-2">
+        Peso: <span>{pokeman.weight} kg</span>
+      </p>
+      <p className="font-bold mr-2">
+        Altura: <span>{pokeman.height} mt</span>
+      </p>
+      <h2 className="text-wxl mt-6 mb-2">Types</h2>
+      {pokeman.types.map((type, index) => (
+        <p key={index}>{type.type.name}</p>
+      ))}
+      <p className="mt-10 text-center">
+        <Link href="/">
+          <a className="text-2xl underline">Al Pokedex</a>
+        </Link>
+      </p>
+    </Layout>
+  );
+}
+
+export async function getServerSideProps({ query }) {
+  const id = query.id;
+  try {
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+    const pokeman = await res.json();
+    const paddedIndex = ("00" + id).slice(-3);
+    const image = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${paddedIndex}.png`;
+    pokeman.image = image;
+    return {
+      props: { pokeman },
+    };
+  } catch (err) {
+    console.error(err);
+  }
+}
